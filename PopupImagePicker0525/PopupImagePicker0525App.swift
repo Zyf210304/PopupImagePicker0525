133278aa3eadbeeed726e90c6964af155f0485bb
张亚飞 <672233580@qq.com>
2022-05-25 16:16:09 +0800

    Initial Commit

---
+++ PopupImagePicker0525/PopupImagePicker0525App.swift
@@ -0,0 +1,17 @@
+//
+//  PopupImagePicker0525App.swift
+//  PopupImagePicker0525
+//
+//  Created by 张亚飞 on 2022/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PopupImagePicker0525App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
